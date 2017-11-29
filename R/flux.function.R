183dbffce5fb5b2cf9a242c456be7421c898d498
Benoit Gauzens <benoit.gauzens@idiv.de>
2017-11-29 17:11:38 +0100

    changing error message in fluxing

--- R/flux.function.R
+++ R/flux.function.R
@@ -176,7 +176,7 @@ fluxing = function(mat, biomasses = NULL, losses, efficiencies, bioms.prefs = TR
   }
   
   if (is.any(F < 0)){
-    stop("impossible to solve the system")
+    stop("model chosen is unable to determine fluxes accoringly to data")
   }
   ### set individual fluxes (each element of ith line from mat.norm is multiplied bu the ith element of F)
   flux.mat = sweep(mat, 2, F, "*")
